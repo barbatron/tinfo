c1c29901c64b395d22facd64e11677b60a2fe3d6
Joakim Olsson <joakim@astrideducation.com>
2023-09-30 10:58:03 +0200

    move sl into subdir

---
+++ src/providers/sl/index.ts
@@ -0,0 +1,21 @@
+import { Config, getConfig } from "../../config.ts";
+import { SlRealtimeClient } from "./slDeparturesClient.ts";
+
+const SL_REALTIME_DEPARTURES_API_URL =
+  "https://api.sl.se/api2/realtimedeparturesV4.json";
+
+export function createSlRealtimeClient(config: Config) {
+  const REALTIME_API_KEY = config.getString("SL_REALTIME_API_KEY", true)!;
+  const SITE_ID = config.getString("SL_SITE_ID", true)!;
+  const JOURNEY_DIRECTION = config.getString("SL_JOURNEY_DIRECTION", true)!;
+  const TIME_WINDOW_MINUTES =
+    config.getNumber("TIME_WINDOW_MINUTES", false) ?? 30;
+
+  return new SlRealtimeClient({
+    apiUrl: SL_REALTIME_DEPARTURES_API_URL,
+    apiKey: REALTIME_API_KEY,
+    siteId: SITE_ID,
+    direction: JOURNEY_DIRECTION,
+    timeWindowMinutes: TIME_WINDOW_MINUTES,
+  });
+}
