2c70e4de1dd74062df4e488cd5399d9898a5fa1b
Joakim Olsson <barbatron@gmail.com>
2025-03-03 19:02:58 +0100

    feat: log.trace gated behind env

--- src/log.ts
+++ src/log.ts
@@ -2,4 +2,6 @@ export const log = {
   ...console,
   debug: (...args: unknown[]) =>
     process.env.DEBUG && console.debug.apply(console, args),
+  trace: (...args: unknown[]) =>
+    process.env.TRACE && console.trace.apply(console, args),
 }; // pino({ ...prettyOpts });
